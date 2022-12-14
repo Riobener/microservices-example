ff654c729c4efe8baea20b8644140eb1afe697aa
Riobener <v.shevchenko@energochain.ru>
2022-12-14 22:44:43 +0300

    Added keycloak auth

--- department-service/src/main/kotlin/com/riobener/departmentservice/controller/DepartmentController.kt
+++ department-service/src/main/kotlin/com/riobener/departmentservice/controller/DepartmentController.kt
@@ -14,14 +14,10 @@ import java.util.*
 @RequestMapping("/departments")
 class DepartmentController(
     private val departmentService: DepartmentService,
-/*    private val tracer: Tracer,*/
 ) {
 
     @PostMapping("/")
     fun saveDepartment(@RequestParam name: String, address: String, code: String): Department {
-/*        val sprintSpan: Span = tracer.buildSpan("saveDepartment")
-            .withTag("name", name).withTag("address", address).withTag("code", code).start()
-        sprintSpan.finish()*/
         return departmentService.saveDepartment(
             Department(
                 id = UUID.randomUUID(),
@@ -34,9 +30,6 @@ class DepartmentController(
 
     @GetMapping("/{id}")
     fun findDepartmentById(@PathVariable("id") departmentId: String): Department? {
-/*        val sprintSpan: Span = tracer.buildSpan("findDepartmentById")
-            .withTag("departmentId", departmentId).start()
-        sprintSpan.finish()*/
         return departmentService.findDepartmentById(UUID.fromString(departmentId))
     }
 }
